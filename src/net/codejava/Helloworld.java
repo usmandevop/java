64c1202b2fe8dedae36e525f0fa6ad8f3dd4f1fa
usmandevop <usman201111@gmail.com>
2021-06-14 12:03:32 -0700

    first java commit

---
+++ src/net/codejava/Helloworld.java
@@ -0,0 +1,11 @@
+package net.codejava;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
